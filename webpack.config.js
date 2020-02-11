e235ac1e13afc7e7d77f440e75d3b37928e0e3f2
martixy <martin.ninov@gmail.com>
2020-02-11 19:15:53 +0200

    Add a copy for config.json.dist>config.json, because the build process fails with it missing.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 //Note: ESM (ES6 modules, i.e. import/export) in config requires using "--config-register esm" param for webpack
 import path from 'path'
+import fs from 'fs'
 import pj2us from 'pj2us-transformer';
 import CopyWebpackPlugin from 'copy-webpack-plugin'
+import EventHooksPlugin from 'event-hooks-webpack-plugin'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
 
 const defaultConf = {
@@ -13,13 +15,26 @@ const defaultConf = {
     },
     plugins: [
         new CleanWebpackPlugin(),
+        new EventHooksPlugin({
+            compile: () => {
+                console.log('Copying config.');
+                if (!fs.existsSync(path.resolve(__dirname, 'config/config.json'))) {
+                    fs.copyFileSync(
+                        path.resolve(__dirname, 'config/config.json.dist'),
+                        path.resolve(__dirname, 'config/config.json')
+                    )
+                } else {
+                    console.log('config.json exists, skipping copy.');
+                }
+            }
+        }),
         new CopyWebpackPlugin([ {
         from: 'package.json',
         to: path.resolve(__dirname, 'dist') + '/local.user.js',
         transform: function(content, resourcePath) {
             return pj2us(content, path.resolve(__dirname, 'dist/main.js'));
         }
-        } ], { })
+        }], { })
     ]
 }
 
